3483e21fdc180504888c5ead7693d99e885266ee
franclobo <fjbl2788@gmail.com>
2024-06-09 08:15:54 -0500

    Edit webhooks

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -3,19 +3,50 @@ import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import paypal from "@paypal/checkout-server-sdk";
 import client from "../../utils/paypal";
+import axios from "axios";
+import crypto from "crypto";
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.text();
-    const signature = headers().get("paypal-auth-algo");
+    const transmissionId = headers().get("paypal-transmission-id");
+    const transmissionTime = headers().get("paypal-transmission-time");
+    const certUrl = headers().get("paypal-cert-url");
+    const transmissionSig = headers().get("paypal-transmission-sig");
+    const webhookId = process.env.PAYPAL_WEBHOOK_ID;
 
-    if (!signature) {
-      return new Response("Invalid signature", { status: 400 });
+    if (
+      !transmissionId ||
+      !transmissionTime ||
+      !certUrl ||
+      !transmissionSig ||
+      !webhookId
+    ) {
+      return new Response(
+        "Invalid signature headers or missing webhook configuration",
+        { status: 400 }
+      );
+    }
+
+    // Fetch PayPal's public certificate
+    const response = await axios.get(certUrl);
+    const cert = response.data;
+
+    // Create the expected signature
+    const expectedSignature = crypto
+      .createVerify("sha256")
+      .update(
+        transmissionId + "|" + transmissionTime + "|" + webhookId + "|" + body
+      )
+      .verify(cert, transmissionSig, "base64");
+
+    if (!expectedSignature) {
+      return new Response("Invalid webhook signature", { status: 400 });
     }
 
     const webhookEvent = JSON.parse(body);
 
-    if (webhookEvent.event_type === "CHECKOUT.ORDER.APPROVED") {
+    if (webhookEvent.event_type === "PAYMENT.SALE.COMPLETED") {
       const orderID = webhookEvent.resource.id;
       const payerEmail = webhookEvent.resource.payer.email_address;
       const purchaseUnits = webhookEvent.resource.purchase_units[0];
@@ -45,7 +76,7 @@ export async function POST(req: NextRequest) {
           shippingAddress: {
             create: {
               name: purchaseUnits.shipping.name.full_name,
-              city: shippingAddress.city,
+              city: shippingAddress.admin_area_2,
               country: shippingAddress.country_code,
               postalCode: shippingAddress.postal_code,
               street: shippingAddress.address_line_1,
@@ -58,7 +89,7 @@ export async function POST(req: NextRequest) {
                 webhookEvent.resource.payer.name.given_name +
                 " " +
                 webhookEvent.resource.payer.name.surname,
-              city: billingAddress.city,
+              city: billingAddress.admin_area_2,
               country: billingAddress.country_code,
               postalCode: billingAddress.postal_code,
               street: billingAddress.address_line_1,
@@ -67,14 +98,15 @@ export async function POST(req: NextRequest) {
           },
         },
       });
+
+      return NextResponse.json({ result: updatedOrder, ok: true });
     }
 
     return NextResponse.json({ result: webhookEvent, ok: true });
   } catch (err) {
     console.error(err);
-
     return NextResponse.json(
-      { message: "Something went wrong", ok: false },
+      { message: "Algo salió mal", ok: false },
       { status: 500 }
     );
   }
