e86dbfca9c09334dab4c3cbc1962f5cc4e00dd83
franclobo <fjbl2788@gmail.com>
2024-06-09 09:39:23 -0500

    edit buffer

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -15,7 +15,10 @@ export const ourFileRouter = {
       const { configId } = metadata.input;
 
       const res = await fetch(file.url);
-      const buffer = await res.arrayBuffer();
+      const arrayBuffer = await res.arrayBuffer();
+
+      // Convert ArrayBuffer to Buffer
+      const buffer = Buffer.from(arrayBuffer);
 
       const imgMetadata = await sharp(buffer).metadata();
       const { width, height } = imgMetadata;
